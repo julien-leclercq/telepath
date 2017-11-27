8177753c1270b675ccc0a56d4e048aff92b6d1ba
julien-leclercq <julien.leclercq@derniercri.io>
2017-11-27 02:13:47 +0100

    add phoenix app and transmission app

---
+++ README.md
@@ -0,0 +1,4 @@
+# TransmissionUi
+
+**TODO: Add description**
+
---
+++ apps/transmission/test/transmission_test.exs
@@ -0,0 +1,8 @@
+defmodule TransmissionTest do
+  use ExUnit.Case
+  doctest Transmission
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
