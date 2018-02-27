7ce0b379cc02d16c571f6e81d79a909eb73b6d4b
Julien Leclercq <julien@MacBook-Pro-de-Julien-3.local>
2018-02-27 12:45:08 +0100

    move decoders from request to data

--- apps/web/assets/elm/Data/Torrent.elm
+++ apps/web/assets/elm/Data/Torrent.elm
@@ -1,16 +1,49 @@
 module Data.Torrent exposing (Torrent, File)
 
+import Json.Decode
+    exposing
+        ( Decoder
+        , string
+        , int
+        , list
+        )
+import Json.Decode.Pipeline exposing (..)
+
 
 type alias Torrent =
     { id : Int
     , name : String
     , downloadDir : String
+    , tracker : String
     , files : List File
     }
 
 
 type alias File =
     { name : String
-    , bytesCompleted : Int -- in bytes
-    , length : Int -- in bytes
+    , bytesCompleted : String -- in bytes
+    , length : String -- in bytes
     }
+
+
+torrentDecoder : Decoder Torrent
+torrentDecoder =
+    decode Torrent
+        |> required "id" int
+        |> required "name" string
+        |> required "downloadDir" string
+        |> required "tracker" string
+        |> required "files" (list fileDecoder)
+
+
+torrentListDecoder : Decoder (List Torrent)
+torrentListDecoder =
+    list torrentDecoder
+
+
+fileDecoder : Decoder File
+fileDecoder =
+    decode File
+        |> required "name" string
+        |> required "bytesCompleted" string
+        |> required "length" string
