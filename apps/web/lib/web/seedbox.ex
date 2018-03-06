fd0518d1fc6c6c68541fca39f450f8ecd6bd0e2a
Julien Leclercq <julien@MacBook-Pro-de-Julien-3.local>
2018-03-06 10:42:31 +0100

    phoenix side seedbox index

---
+++ apps/web/lib/web/seedbox.ex
@@ -0,0 +1,45 @@
+defmodule Web.Seedbox do
+  def list do
+    {:ok,
+     [
+       %{
+         id: 1,
+         url: "",
+         port: nil,
+         remote: true
+       }
+     ]}
+  end
+
+  #   torrents : List Torrent
+  # torrents =
+  #     [ { id = 1
+  #       , name = "torrent 1"
+  #       , downloadDir = "/user/Downloads/torrents"
+  #       , tracker = "such tracker"
+  #       , files =
+  #             [ { name = "file 1", bytesCompleted = "45", length = "176" }
+  #             , { name = "file 2", bytesCompleted = "38", length = "3456" }
+  #             ]
+  #       }
+  #     , { id = 2
+  #       , name = "torrent 14"
+  #       , tracker = "very tracker"
+  #       , downloadDir = "/user/Downloads/torrents"
+  #       , files = []
+  #       }
+  #     , { id = 3
+  #       , name = "torrent 3"
+  #       , tracker = "much pirate"
+  #       , downloadDir = "/user/Downloads/torrents"
+  #       , files = []
+  #       }
+  #     ]
+
+  # seedbox : Seedbox
+  # seedbox =
+  #     Remote
+  #         { torrents = torrents
+  #         , settings = { url = "", port_ = Nothing }
+  #         }
+end
--- apps/web/lib/web_web/router.ex
+++ apps/web/lib/web_web/router.ex
@@ -2,27 +2,28 @@ defmodule WebWeb.Router do
   use WebWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", WebWeb do
-    pipe_through :browser # Use the default browser stack
+    # Use the default browser stack
+    pipe_through(:browser)
 
-    get "/", PageController, :index
+    get("/", PageController, :index)
   end
 
   # Other scopes may use custom stacks.
   scope "/api", WebWeb do
-    pipe_through :api
+    pipe_through(:api)
 
-    get "/seedbox", Api.SeedboxController, :index
+    get("/seedboxes", Api.SeedboxController, :index)
   end
 end
