20669bb7c1ca8a97244ed234499843958b978035
Julien Leclercq <julien@MacBook-Pro-de-Julien-3.local>
2018-03-06 10:44:36 +0100

    add elixir formatter config

--- apps/web/config/config.exs
+++ apps/web/config/config.exs
@@ -6,16 +6,14 @@
 use Mix.Config
 
 # General application configuration
-config :web,
-  namespace: Web
+config :web, namespace: Web
 
 # Configures the endpoint
 config :web, WebWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "rO7BXmfmnblTorOglPkF9krVUcvNdXgS5RIaAVSzp138jQ0PZ4HHqjImxW5laLd0",
   render_errors: [view: WebWeb.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: Web.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: Web.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
 config :logger, :console,
@@ -24,4 +22,4 @@ config :logger, :console,
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
