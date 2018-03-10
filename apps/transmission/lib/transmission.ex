8c70ac048a2522597d3bf3af2b7632a0e5017c44
julien-leclercq <julien.leclercq@gmx.fr>
2018-03-10 17:16:23 +0100

    Get session API Call (#4)
    * transmission api : add httpoison
    
    * remove obsolete derive attributes
    
    * add kaur
    
    * add api session get function
    
    * add exenv elixir version file
    
    * add credo
    
    * exclude assets folder in credo config
    
    * add module doc for undocumented modules
    
    * fix credo issues
    
    * transmission api : add httpoison
    
    * remove obsolete derive attributes
    
    * add api session get function

--- apps/transmission/lib/transmission.ex
+++ apps/transmission/lib/transmission.ex
@@ -1,4 +1,10 @@
 defmodule Transmission do
+  alias Kaur.Result
+  use HTTPoison.Base
+
+  @get_session "session-get"
+  @session_id_key "X-Transmission-Session-Id"
+
   @moduledoc """
   Documentation for Transmission.
   """
@@ -8,8 +14,7 @@ defmodule Transmission do
     Provides a data structure for Transmission api requests
     """
 
-    @derive Poison.Encoder
-    defstruct [:tag, :method, :arguments]
+    defstruct [:method, arguments: []]
   end
 
   defmodule Response do
@@ -17,7 +22,160 @@ defmodule Transmission do
     Provides a data structure for Transmission api responses
     """
 
-    @derive Poison.Encoder
-    defstruct [:tag, :result, :arguments]
+    defstruct [:result, :arguments]
+  end
+
+  def get_session(%{host: _host, port: _port} = seedbox, options \\ []) do
+    %Request{
+      method: @get_session
+    }
+    |> send_request(seedbox, [], options)
+    |> Result.and_then(&handle_response/1)
+  end
+
+  defp send_request(
+         %Request{} = request,
+         %{auth: %{user: user, password: password}} = seedbox,
+         headers,
+         options
+       ) do
+    seedbox = Map.delete(seedbox, :auth)
+    options = [hackney: [basic_auth: {user, password}]] ++ options
+
+    send_request(request, seedbox, headers, options)
+    |> Result.and_then(&handle_response/1)
+    |> Result.or_else(fn {:conflict, header} ->
+      send_request(request, seedbox, [header | headers], options)
+    end)
+  end
+
+  defp send_request(%Request{} = request, %{host: _host, port: _port} = seedbox, headers, options) do
+    seedbox
+    |> build_url
+    |> post(request, headers, options)
+  end
+
+  defp process_request_body(request) do
+    Poison.encode!(request)
+  end
+
+  defp process_request_options(options) do
+    [follow_redirect: true] ++ options
+  end
+
+  defp process_url(url) do
+    "#{url}/transmission/rpc"
+  end
+
+  defp build_url(%{host: host, port: port}) do
+    "#{host}:#{port}"
+  end
+
+  defp handle_response(%HTTPoison.Response{status_code: status_code} = response)
+       when status_code >= 400 do
+    case status_code do
+      401 ->
+        Result.error(:unauthorized)
+
+      500 ->
+        Result.error(:fatal_error)
+
+      409 ->
+        Result.error({:conflict, find_transmission_header(response)})
+
+      _ ->
+        Result.error(status_code)
+    end
+  end
+
+  defp handle_response(%HTTPoison.Response{body: body}) do
+    Poison.decode(body, as: %Response{})
+    |> Result.and_then(fn
+      %Response{result: "success"} = response -> {:ok, response}
+      %Response{result: reason} -> {:error, reason}
+    end)
+  end
+
+  defp find_transmission_header(response) do
+    response.headers
+    |> Enum.find(fn {key, _} -> key == @session_id_key end)
+  end
+
+
+  def get_session(%{host: _host, port: _port} = seedbox, options \\ []) do
+    %Request{
+      method: @get_session
+    }
+    |> send_request(seedbox, [], options)
+    |> Result.and_then(&handle_response/1)
+  end
+
+  defp send_request(
+         %Request{} = request,
+         %{auth: %{user: user, password: password}} = seedbox,
+         headers,
+         options
+       ) do
+    seedbox = Map.delete(seedbox, :auth)
+    options = [hackney: [basic_auth: {user, password}]] ++ options
+
+    send_request(request, seedbox, headers, options)
+    |> Result.and_then(&handle_response/1)
+    |> Result.or_else(fn {:conflict, header} ->
+      send_request(request, seedbox, [header | headers], options)
+    end)
+  end
+
+  defp send_request(%Request{} = request, %{host: _host, port: _port} = seedbox, headers, options) do
+    seedbox
+    |> build_url
+    |> post(request, headers, options)
+  end
+
+  defp process_request_body(request) do
+    Poison.encode!(request)
+  end
+
+  defp process_request_options(options) do
+    [follow_redirect: true] ++ options
+  end
+
+  defp process_url(url) do
+    "#{url}/transmission/rpc"
+  end
+
+  defp build_url(%{host: host, port: port}) do
+    "#{host}:#{port}"
+  end
+
+  defp handle_response(%HTTPoison.Response{status_code: status_code} = response)
+       when status_code >= 400 do
+    case status_code do
+      401 ->
+        Result.error(:unauthorized)
+
+      500 ->
+        Result.error(:fatal_error)
+
+      409 ->
+        Result.error({:conflict, find_transmission_header(response)})
+
+      _ ->
+        Result.error(status_code)
+    end
+  end
+
+  defp handle_response(%HTTPoison.Response{body: body}) do
+    Poison.decode(body, as: %Response{})
+    |> Result.and_then(fn
+      %Response{result: "success"} = response -> {:ok, response}
+      %Response{result: reason} -> {:error, reason}
+    end)
+  end
+
+  defp find_transmission_header(response) do
+    response.headers
+    |> Enum.find(fn {key, _} -> key == @session_id_key end)
+
   end
 end
