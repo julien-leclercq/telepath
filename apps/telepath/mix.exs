bfc222e7d595db386eabc8dedaaccdd51a0241b0
julien-leclercq <julien.leclercq@gmx.fr>
2018-03-14 10:57:17 +0100

    application behaviour for telepath

--- apps/telepath/mix.exs
+++ apps/telepath/mix.exs
@@ -14,7 +14,8 @@ defmodule Telepath.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {Telepath.Application, []}
     ]
   end
 
