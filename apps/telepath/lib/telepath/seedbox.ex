700522ce8e810900f39e0f503cdd5efb0fecaed6
julien-leclercq <julien.leclercq@gmx.fr>
2018-03-14 10:48:58 +0100

    define ecto schema for seedbox

--- apps/telepath/lib/telepath/seedbox.ex
+++ apps/telepath/lib/telepath/seedbox.ex
@@ -2,13 +2,14 @@ defmodule Telepath.Seedbox do
   @moduledoc """
   This module provides data structure and functions to manipulate seedboxes
   """
+  use Ecto.Schema
 
-  defstruct [
-    :id,
-    :url,
-    :port,
-    :name
-  ]
+  embedded_schema do
+    field(:host, :string)
+    field(:name, :string)
+    field(:port, :string)
+    field(:accessible, :boolean)
+  end
 
   def update(seedbox, params) do
     {:ok, Map.merge(seedbox, params)}
