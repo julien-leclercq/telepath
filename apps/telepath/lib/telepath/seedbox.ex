7cb09dab03c74971670e6a2a991fa579f414f5b1
julien-leclercq <julien.leclercq@gmx.fr>
2018-03-20 12:34:50 +0100

    update seedbox data model

--- apps/telepath/lib/telepath/seedbox.ex
+++ apps/telepath/lib/telepath/seedbox.ex
@@ -8,11 +8,11 @@ defmodule Telepath.Seedbox do
   use Ecto.Schema
 
   embedded_schema do
-    field :accessible, :boolean, default: false
-    field :host, :string
-    field :name, :string, default: ""
-    field :port, :string
-    field :remote, :boolean, default: true
+    field(:accessible, :boolean, default: false)
+    field(:host, :string)
+    field(:name, :string, default: "")
+    field(:port, :string)
+    field(:remote, :boolean, default: true)
   end
 
   def create(seedbox_params) do
