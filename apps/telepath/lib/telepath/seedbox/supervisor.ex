d94d67d788980c8ccd9fe2b9f28d079eb64fbcc1
julien-leclercq <julien.leclercq@gmx.fr>
2018-03-20 16:33:40 +0100

    seedbox index

--- apps/telepath/lib/telepath/seedbox/supervisor.ex
+++ apps/telepath/lib/telepath/seedbox/supervisor.ex
@@ -8,7 +8,7 @@ defmodule Telepath.Seedbox.Supervisor do
   use DynamicSupervisor
 
   def start_link() do
-    DynamicSupervisor.start_link(__MODULE__, [])
+    DynamicSupervisor.start_link(__MODULE__, [], name: __MODULE__)
   end
 
   def init(_) do
