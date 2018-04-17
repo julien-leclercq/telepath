7ae29b2e86677fa44317bf816f0c259e5df56180
julien-leclercq <julien.leclercq@gmx.fr>
2018-04-17 13:58:14 +0200

    update torrent view

--- apps/web/assets/elm/Views/TorrentList/Item.elm
+++ apps/web/assets/elm/Views/TorrentList/Item.elm
@@ -1,7 +1,7 @@
 module Views.TorrentList.Item exposing (..)
 
 import Data.Torrent exposing (File, Torrent)
-import Html exposing (Html, button, div, input, label, progress, span, text)
+import Html exposing (Html, button, div, input, label, p, progress, span, text)
 import Html.Attributes as Attrs
 
 
@@ -14,29 +14,32 @@ view torrent =
         showInfosCheckId =
             "show-infos-torrent" ++ (toString torrent.id)
     in
-        div [ Attrs.class "box torrent" ]
+        div [ Attrs.class "card torrent" ]
             [ input [ Attrs.type_ "checkbox", Attrs.class "show-files", Attrs.id showFileCheckId ] []
             , input [ Attrs.type_ "checkbox", Attrs.class "show-infos", Attrs.id showInfosCheckId ] []
-            , div [ Attrs.class "level torrent-header" ]
-                [ div [ Attrs.class "level-left" ] [ span [ Attrs.class "level-item" ] [ text torrent.name ] ]
-                , div [ Attrs.class "level-right" ]
+            , div [ Attrs.class "level" ] [ span [ Attrs.class "level-left level-item" ] [ text torrent.name ] ]
+            , div [ Attrs.class "card-footer torrent-header" ]
+                [ div [ Attrs.class "level-right" ]
                     [ label [ Attrs.for showFileCheckId, Attrs.class "button show-files-label level-item" ] []
                     , label [ Attrs.for showInfosCheckId, Attrs.class "button show-infos-label level-item" ] []
                     ]
                 ]
             , filesView torrent
-            , infosView torrent
+
+            -- , infosView torrent
             ]
 
 
 fileView : File -> Html msg
 fileView file =
-    div [ Attrs.class "level columns" ]
-        [ div [ Attrs.class "level-left column is-one-fifth" ]
-            [ span [ Attrs.class "file-title level-item" ] [ text file.name ] ]
-        , div [ Attrs.class "level-right column" ]
-            [ div [ Attrs.class "level-item" ]
-                [ progress [ Attrs.class "progress", Attrs.value file.bytesCompleted, Attrs.max file.length ] []
+    div [ Attrs.class "media" ]
+        [ div [ Attrs.class "media-content" ]
+            [ div [ Attrs.class "level" ]
+                [ span [ Attrs.class "file-title" ] [ text file.name ] ]
+            , div []
+                [ div [ Attrs.class "level" ]
+                    [ progress [ Attrs.class "progress", file.bytesCompleted |> toString |> Attrs.value, Attrs.max <| toString file.length ] []
+                    ]
                 ]
             ]
         ]
@@ -47,5 +50,9 @@ filesView torrent =
     div [ Attrs.class "files media" ] [ div [ Attrs.class "media-content" ] (List.map fileView torrent.files) ]
 
 
+
+-- infosView : Torrent -> Html msg
+
+
 infosView torrent =
-    div [ Attrs.class "infos media" ] [ text torrent.tracker ]
+    div [ Attrs.class "infos media" ] (List.map (\tracker -> p [] [ text tracker ]) torrent.trackers)
