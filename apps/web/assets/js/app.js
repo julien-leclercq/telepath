8f4d7c4dbf6572fd57038ce7795dcc2adc305fc8
julien-leclercq <julien.leclercq@gmx.fr>
2018-10-08 11:40:50 +0200

    mvp for elm interface

--- apps/web/assets/js/app.js
+++ apps/web/assets/js/app.js
@@ -2,5 +2,36 @@ import "phoenix_html"
 import '../css/app.sass'
 import Elm from '../elm/Main.elm'
 
+let track
+let playing = false
 const container = document.querySelector("#app")
 const app = Elm.Main.embed(container)
+console.log(app)
+
+app.ports.playerCmdOut.subscribe(function (ctrl) {
+  const actions = {
+    "playTrack": (ctrl) => swapTrack(ctrl.track.route),
+    "togglePlay": (_ctrl) => {
+      if (track) {
+        track.paused ? track.play() : track.pause()
+      }
+    }
+  }
+
+  actions[ctrl.action](ctrl)
+  console.log(ctrl)
+})
+
+function swapTrack(trackPath) {
+  if (track) {
+    track.pause()
+  }
+  track = new Audio(trackPath)
+  track.ontimeupdate = (e) => handleTimeChange(e.target)
+  track.play()
+  playing = true
+}
+
+function handleTimeChange(track) {
+  app.ports.playerCmdIn.send(track.currentTime)
+}
--- apps/web/lib/web/track.ex
+++ apps/web/lib/web/track.ex
@@ -1,8 +1,16 @@
 defmodule Web.Track do
   alias Kaur.Result
+  alias Koop.Schema.Track
 
   def list(_params) do
-    DB.Repo.all(Koop.Schema.Track)
+    DB.Repo.all(Track)
     |> Result.ok()
+  end
+
+  def get_path(track_id) do
+    track = Track
+    |> DB.Repo.get(track_id)
+
+    track.filename
   end
 end
